d17a472c0a4ad205fb5ba7d683b7d53a64d38a80
pscore23 <rubik2357x@gmail.com>
2022-09-19 12:15:20 +0900

    更新

--- script/footer.js
+++ script/footer.js
@@ -1,5 +1,9 @@
 document.getElementById("footer").innerHTML = `<footer id="footer">
     <p>
+        <small>
+            <a href="/privacy/index.html">プライバシーポリシー</a>
+        </small>
+        <br />
         © 2022 All rights reserved by <a href="https://github.com/pscore23">pscore23</a>.
     </p>
 </footer>`;
