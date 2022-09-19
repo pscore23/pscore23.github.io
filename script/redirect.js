fbc3ec67b903d6ac3c54ba9f8766e2ffdd289500
pscore23 <rubik2357x@gmail.com>
2022-09-19 10:38:28 +0900

    更新

--- script/redirect.js
+++ script/redirect.js
@@ -1,7 +1,5 @@
-function resetLocation() {
+function resetLocation(a, b) {
     alert("お探しのページが見つかりません - トップページにリダイレクトします")
 
     window.location.href = "https://pscore23.github.io/index.html"
-}
-
-resetLocation()
+};
