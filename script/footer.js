eb8200cedfb4d155f6c806a55ef51f5c64d86367
pscore23 <rubik2357x@gmail.com>
2022-10-22 12:29:04 +0900

    [delete]

--- script/footer.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("footer").innerHTML = `<footer id="footer">
-    <p>
-        <small>
-            <a href="/privacy/index.html">プライバシーポリシー</a>
-        </small>
-        <br />
-        © 2022 All rights reserved by <a href="https://github.com/pscore23">pscore23</a>.
-    </p>
-</footer>`;
