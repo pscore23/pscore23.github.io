8fadb27956cf46bb2093cc5e608495054b81eb58
pscore23 <rubik2357x@gmail.com>
2022-10-23 12:37:29 +0900

    [update]

--- script/include.js
+++ script/include.js
@@ -38,6 +38,11 @@ function set() {
                     <a>
                     </a>
                 </li>
+                <li>
+                    <a href="javascript:void(0);" onclick="window.location.href = window.location.href">
+                        このページをスーパーリロード
+                    </a>
+                </li>
             </ul>
         </nav>
     </header>
