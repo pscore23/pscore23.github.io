83b5007a825517a8c038baedef18231e16eedec9
pscore23 <rubik2357x@gmail.com>
2022-10-24 20:08:23 +0900

    [update]

--- script/set.js
+++ script/set.js
@@ -2,4 +2,13 @@ import { insertHeaderAndFooter } from "./include.js";
 
 window.onload = () => {
     insertHeaderAndFooter();
-}
+
+    let mode = localStorage.getItem("darkmode");
+
+    if (mode == "/stylesheet.css") {
+        document.getElementById("dark").href = "/stylesheet.css"
+
+    } else if (mode == "/dark_stylesheet.css") {
+        document.getElementById("dark").href = "/dark_stylesheet.css"
+    }
+};
