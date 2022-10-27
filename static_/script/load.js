786147e361bd311c531bd55397235b9986f2ebb9
pscore23 <rubik2357x@gmail.com>
2022-10-27 20:41:37 +0900

    [update]

--- static_/script/load.js
+++ static_/script/load.js
@@ -1,3 +1,4 @@
 function superReload() {
-    window.location.href = window.location.href
+    window.location.href = window.location.href // スーパーリロードの方法が分からないからとりあえずどっちも書いてる
+    window.location.reload(true) // もしかして:　JavaScript じゃスーパーリロード出来ない
 };
