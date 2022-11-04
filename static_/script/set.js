c60679d1d3b092bf014921a80bb3c6678cb512fd
pscore23 <rubik2357x@gmail.com>
2022-11-04 18:35:00 +0900

    [update]

--- static_/script/set.js
+++ static_/script/set.js
@@ -1,20 +1,36 @@
 import { insertHeaderAndFooter } from "./include.js";
 
+function setMode(mode) {
+    if (mode == "/stylesheet.css") {
+        document.getElementById("dark").href = "/stylesheet.css"
+    } else if (mode == "/dark_stylesheet.css") {
+        document.getElementById("dark").href = "/dark_stylesheet.css"
+    }
+}
+
+function setLinks() {
+    let urls = document.getElementsByTagName("a");
+
+    for (let i = 0; urls.length; i++) {
+        let link_ = document.createElement("link");
+
+        link_.rel = "dns-prefetch"
+        link_.href = urls[i].href
+
+        document.head.appendChild(link_)
+    }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     document.querySelector("body").setAttribute("style", "pointer-events: none;");
 })
 
 window.onload = () => {
-    let mode = localStorage.getItem("darkmode");
-
-    if (mode == "/stylesheet.css") {
-        document.getElementById("dark").href = "/stylesheet.css"
-
-    } else if (mode == "/dark_stylesheet.css") {
-        document.getElementById("dark").href = "/dark_stylesheet.css"
-    }
+    setMode(localStorage.getItem("darkmode"));
 
     insertHeaderAndFooter();
 
     document.querySelector("body").setAttribute("style", "pointer-events: auto;")
+
+    setLinks()
 };
